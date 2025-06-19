77a88dc8dfe9aefbfa79dfe9a3ccf1249a5df9f3
Houda <houdijijo@gmail.com>
2025-06-19 20:29:19 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  base: '/Portfolio-Houda/',
 })
